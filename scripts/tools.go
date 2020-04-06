a9df00baecc626f2e867f5aa9e17f8275ed9e7ed
Damien Grisonnet <dgrisonn@redhat.com>
2020-04-06 12:49:51 +0200

    mod: add tooling dependencies
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

--- scripts/tools.go
+++ scripts/tools.go
@@ -5,5 +5,9 @@
 package tools
 
 import (
+	_ "github.com/brancz/gojsontoyaml"
+	_ "github.com/campoy/embedmd"
+	_ "github.com/google/go-jsonnet/cmd/jsonnet"
+	_ "github.com/google/go-jsonnet/cmd/jsonnetfmt"
 	_ "github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb"
 )
