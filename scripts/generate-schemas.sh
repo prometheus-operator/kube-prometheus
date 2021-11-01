89e81dded15bf3197cc18a1eacf18942d2ea41a3
paulfantom <pawel@krupa.net.pl>
2021-11-01 09:07:12 +0100

    scripts: modify schemas generator to include changes from https://github.com/prometheus-operator/prometheus-operator/pull/4346
    Signed-off-by: paulfantom <pawel@krupa.net.pl>

--- scripts/generate-schemas.sh
+++ scripts/generate-schemas.sh
@@ -8,6 +8,6 @@ cd ./$(git rev-parse --show-cdup)
 rm -rf "$DIR"
 mkdir "$DIR"
 
-for crd in vendor/prometheus-operator/*-crd.libsonnet; do
-	jq '.spec.versions[0].schema.openAPIV3Schema' < "$crd" > "$DIR/$(basename "$crd" | sed 's/-crd.libsonnet/.json/')"
+for crd in vendor/prometheus-operator/*-crd.json; do
+	jq '.spec.versions[0].schema.openAPIV3Schema' < "$crd" > "$DIR/$(basename "$crd" | sed 's/s-crd//;s/prometheuse/prometheus/')"
 done
